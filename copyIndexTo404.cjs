024bdec79e9c9b43f92f7fa9c508734fe6034df9
master <time_and_work@mail.ru>
2025-10-07 23:39:01 +0300

    micro fix

--- copyIndexTo404.cjs
+++ copyIndexTo404.cjs
@@ -1,11 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 
-const distFolder = path.resolve('./dist');
-const indexFile = path.join(distFolder, 'index.html');
-const errorFile = path.join(distFolder, '404.html');
+const distPath = path.resolve('./dist');
+const indexHtml = path.join(distPath, 'index.html');
+const errorHtml = path.join(distPath, '404.html');
 
-fs.copyFile(indexFile, errorFile, (err) => {
-  if (err) console.error(err);
-  else console.log('404.html успешно создан!');
-});
+const redirectContent = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Redirecting...</title>
+  <script type="text/javascript">
+    var path = window.location.pathname;
+    window.location.replace("/hhRouteErrorReact/index.html#" + path);
+  </script>
+</head>
+<body>
+Redirecting...
+</body>
+</html>
+`;
+
+fs.writeFileSync(errorHtml, redirectContent, 'utf-8');
+console.log('404.html создан успешно!');
